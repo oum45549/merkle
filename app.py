b74dcca50ba4a904eb0a6b9e17beefabfddc76d7
Dani <danielrobledo2001@gmail.com>
2024-04-22 20:56:05 +0200

    Añadido botón que no hace nada y eliminado dependencias sin uso

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import math
-import random
 
+st.image('MERKLE2x.png')
 st.title('MERKLE -equipo 4')
 
 #
@@ -12,6 +9,8 @@ a = st.radio('Analizar noticia desde:',["URL","Texto"])
 # si se ha elegido analizar desde URL
 if a == "URL":
     url_noticia=st.text_input("Introduce la dirección web de la noticia")
+    st.button('Prueba URL')
 else: # si se ha elegido analizar desde texto
     txt_noticia=st.text_area("Introduce el texto de la noticia",value="Escribe o pega aquí el texto de la noticia")
+    st.button('Prueba HTML')
 
