1f355dc870906ea31adc861edf245a4a056ee1b9
Dani <danielrobledo2001@gmail.com>
2024-04-22 19:21:46 +0200

    página vacía pero se puede poner URL y texto

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import math
+import random
+
+st.title('MERKLE -equipo 4')
+
+#
+a = st.radio('Analizar noticia desde:',["URL","Texto"])
+
+# si se ha elegido analizar desde URL
+if a == "URL":
+    url_noticia=st.text_input("Introduce la dirección web de la noticia")
+else: # si se ha elegido analizar desde texto
+    txt_noticia=st.text_area("Introduce el texto de la noticia",value="Escribe o pega aquí el texto de la noticia")
 
